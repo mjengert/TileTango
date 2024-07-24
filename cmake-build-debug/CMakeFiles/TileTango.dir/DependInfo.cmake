1903662f6f28adabab6d73530547ed7163661345
Natalie Poche <nataliealena03@gmail.com>
2024-07-24 14:15:29 -0400

    Added welcome window GUI, need to fix button spacing later.

---
+++ cmake-build-debug/CMakeFiles/TileTango.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Owner/CLionProjects/Data_Structures/TileTango/BFS.cpp" "CMakeFiles/TileTango.dir/BFS.cpp.obj" "gcc" "CMakeFiles/TileTango.dir/BFS.cpp.obj.d"
+  "C:/Users/Owner/CLionProjects/Data_Structures/TileTango/IDA.cpp" "CMakeFiles/TileTango.dir/IDA.cpp.obj" "gcc" "CMakeFiles/TileTango.dir/IDA.cpp.obj.d"
+  "C:/Users/Owner/CLionProjects/Data_Structures/TileTango/SlidingBoards.cpp" "CMakeFiles/TileTango.dir/SlidingBoards.cpp.obj" "gcc" "CMakeFiles/TileTango.dir/SlidingBoards.cpp.obj.d"
+  "C:/Users/Owner/CLionProjects/Data_Structures/TileTango/main.cpp" "CMakeFiles/TileTango.dir/main.cpp.obj" "gcc" "CMakeFiles/TileTango.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/TileTango.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TileTango.dir/BFS.cpp.obj"
+  "CMakeFiles/TileTango.dir/BFS.cpp.obj.d"
+  "CMakeFiles/TileTango.dir/IDA.cpp.obj"
+  "CMakeFiles/TileTango.dir/IDA.cpp.obj.d"
+  "CMakeFiles/TileTango.dir/SlidingBoards.cpp.obj"
+  "CMakeFiles/TileTango.dir/SlidingBoards.cpp.obj.d"
+  "CMakeFiles/TileTango.dir/main.cpp.obj"
+  "CMakeFiles/TileTango.dir/main.cpp.obj.d"
+  "TileTango.exe"
+  "TileTango.exe.manifest"
+  "TileTango.pdb"
+  "libTileTango.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TileTango.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
